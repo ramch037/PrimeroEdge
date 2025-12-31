8c63318be7cf2869f551b7bb93329eaeaf04b15c
ramch037 <ramakrishna.ch037@gmail.com>
2025-12-31 10:41:53 +0530

    waits added for user managment test

--- page_manage_users.py
+++ page_manage_users.py
@@ -1,5 +1,7 @@
 from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class ManageUsersPage:
@@ -11,9 +13,14 @@ class ManageUsersPage:
         self.search_user_username_textbox = (By.ID, 'ctl00_UserContentArea_ucUserSearch_UsersRadGrid_ctl00_ctl02_ctl02_FilterTextBox_UserName')
 
     def click_user_add_button(self):
-        self.driver.find_element(*self.user_add_button).click()
+        ele = self.driver.find_element(*self.user_add_button)
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(expected_conditions.element_to_be_clickable(ele))
+        ele.click()
 
     def type_in_and_press_enter_to_search_user_username_textbox(self, username):
         ele = self.driver.find_element(*self.search_user_username_textbox)
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(expected_conditions.element_to_be_clickable(ele))
         ele.send_keys(username)
         ele.send_keys(Keys.ENTER)
